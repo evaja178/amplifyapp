72c52fba82a518a9cec95101b3a426e9004bd0c4
evaja178 <36187937+evaja178@users.noreply.github.com>
2021-04-27 21:05:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We are absolutely delighted to announce to you that you have won all inclusive stay at our fabulous, very well known private resort The Royals of Cam. Your stay has been booked from 11th to 13th of May and all you need to do is to show up. We will take care of everything else. 
+          We are absolutely delighted to announce to you that you have won all inclusive stay at our fabulous, very well known private resort The Royals of Cam. ~{"\n"}
+    Your stay has been booked from 11th to 13th of May and all you need to do is to show up. We will take care of everything else. ~{"\n"}
 
-          Please, contact us on 07871453675 to claim your prize and to arrange the details of your stay.
+          Please, contact us on 07871453675 to claim your prize and to arrange the details of your stay.~{"\n"}
 
           We can’t wait for your visit! 
 
