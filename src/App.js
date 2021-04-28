f26aaaa8b9eb21e687b7fc05489886eb304221b4
evaja178 <36187937+evaja178@users.noreply.github.com>
2021-04-28 15:44:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We are absolutely delighted to announce you that you won all inclusive stay at our fabulous, very well known private resort The Royals of Cam. {"\n"}
-    Your stay is booked from 11th to 13th of May and all you need to do is to show up. Our fabulous team will take care of everything else. {"\n"}
+        <p1>
+          We are absolutely delighted to announce you that you won all inclusive stay at our fabulous, very well known private resort The Royals of Cam.
+        </p1> 
+        <p2>    
+          Your stay is booked from 11th to 13th of May and all you need to do is to show up. Our fabulous team will take care of everything else. 
 
-          Please, contact us on 07871453675 to claim your prize and to arrange the details of your stay. {"\n"}
+          Please, contact us on 07871453675 to claim your prize and to arrange the details of your stay. 
 
           We can’t wait for your visit! 
 
-        </p>
+        </p2>
         <a
           className="App-link"
           href="https://www.google.com/maps/place/2120+Water+St,+Chesterton,+Cambridge+CB4+1XZ/@52.217951,0.1449317,18z/data=!4m5!3m4!1s0x47d87058db12df2f:0x6c8dc158c741fdd8!8m2!3d52.2186!4d0.146969"
